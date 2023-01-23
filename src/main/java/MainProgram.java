62402d72d6a54b55e42f5b3a063ed449baef05e3
ThinkStu <1914493943@qq.com>
2023-01-23 20:19:37 +0800

    V1.0版本

---
+++ src/main/java/MainProgram.java
@@ -0,0 +1,59 @@
+import cn.hutool.core.io.file.FileReader;
+import cn.hutool.core.io.file.*;
+import cn.hutool.core.util.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+
+/**
+ * @author : ThinkStu
+ * @since : 2023/1/23, 17:48, 周一
+ **/
+public class MainProgram {
+    public static void main(String[] args) {
+        // 传入 fileName 文件名，文件应位于 resources 目录下
+        imageTrasation("测试文稿.md");
+    }
+
+    private static void imageTrasation(String fileName) {
+        Integer count   = 0;
+        Matcher matcher;
+        // 正则表达式适配 原图片链接
+        Pattern pattern = Pattern.compile(".*<img.*=\"(.*)\".*=\"(.*)\" .*zoom:(\\d+)%.*/>.*");
+        // 文件应置于 src/main/resources/目录下
+        String basePath   = "src/main/resources/";
+        // 源文件与目标文件
+        File   sourceFile = new File(basePath+fileName);
+        File   targetFile = new File(basePath + "CSDN-" + sourceFile.getName());
+        // 目标文件若存在，则删除、新建
+        if (targetFile.exists()) {
+            targetFile.delete();
+        }
+        FileReader   fileReader = new FileReader(sourceFile);
+        List<String> lines      = fileReader.readLines();
+        FileAppender appender   = new FileAppender(targetFile, 16, true);
+        // 开始逐行遍历，利用正则表达式进行适配
+        for (String line : lines) {
+            matcher = pattern.matcher(line);
+            if (matcher.matches()) {
+                Integer rate = Integer.parseInt(matcher.group(3));
+                // 对图片尺寸作适配
+                if (rate <= 30) {
+                    rate = 360;
+                } else if (rate <= 70) {
+                    rate = 480;
+                } else {
+                    rate = 640;
+                }
+                line = StrUtil.format("![{}]({} ={}x)",
+                        matcher.group(2), matcher.group(1), rate);
+                count++;
+            }
+            appender.append(line);
+        }
+        appender.flush();
+        System.out.println("执行完毕，一共替换：" + count + " 行");
+    }
+}
